58242f19d95497c9c7ef20e4dd1d51d2314562d8
johnny5550822 <johnny5550822@gmail.com>
2015-03-06 11:54:58 -0800

    add toolbox function

---
+++ knn/kchbox_knn.m
@@ -0,0 +1,15 @@
+% KNN on a test data
+% Input: test_d(1xf), train_d(mxf), train_label(1xm), where f is the number
+% of features, m is the number of training data.
+
+
+function prediction = kchbox_knn(k,test_d,train_d,train_label)
+
+
+
+
+end
+
+
+
+
