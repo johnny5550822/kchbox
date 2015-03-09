683984bc700c17bc6cec9a9463f49e4d36113bd9
johnny <johnny5550822@gmail.com>
2015-03-09 15:18:10 -0700

    Prepare data for the project

--- examples/ucla_cs260/knn_main.m
+++ examples/ucla_cs260/knn_main.m
@@ -5,44 +5,40 @@
 % Number of training data: 39 patients, 20 with label 0, and 19 with label
 % 1
 
-%% Step 1 Creating training data (random) for 2 clusters using gaussian distribution.
-% Assuming there are two clusters with two dimenions
-
+% WIll attempt different method and see which one give the best result.
+% Have a feature generation function to generate different features
+%% Step 0 Creating training data (random) for 2 clusters using gaussian distribution.
 clear all;
 clc;
+addpath data/
 
-% parameters
-num_data = 20;
-train_label = zeros(1,num_data*2);
+%read diastolic data, nxm, where m is the number of data point(feature).
+%And n is the number of patients
+raw_d = xlsread('combine_dia');   
+dia = raw_d(:,2:end);
 
-% ###### cluster 1
-% Define cluster
-mean_1 = 2;
-sigma_1 = 0.9;
-x1 = normrnd(mean_1,sigma_1,[1 num_data]);
-y1 = normrnd(mean_1,sigma_1,[1 num_data]);
-cluster_1 = [x1 ; y1]';
-% Define label
-train_label(1:num_data) = 1;
+%read systolic data
+raw_s = xlsread('combine_sys');   
+systo = raw_s(:,2:end);
 
-% ##### cluster 2
-% Define cluster
-mean_2 = 5;
-sigma_2 = 0.9;
-x2 = normrnd(mean_2,sigma_2,[1 num_data]);
-y2 = normrnd(mean_2,sigma_2,[1 num_data]);
-cluster_2 = [x2 ; y2]';
-% Define label
-train_label(num_data+1:end) = 2;
+%read label
+train_label = xlsread('label');
+train_label = train_label(:,2);
 
-% Plot 
-plot(cluster_1(:,1),cluster_1(:,2),'bo',cluster_2(:,1),cluster_2(:,2),'rx');
+% NOTE: One possible step here is to normalize the data series for each
+% patient. However, the hypoethesis is that the magnitude of the pressure
+% matter relative to different type of patients. So, I did not perform this
+% step
+%% *********************METHOD 1 Simple knn with Euclidean distance*******
 
-%join two clusters
-train_d = [cluster_1; cluster_2];
-%% Step 2 Define k
+%% Step 1. Feature generation
+fv = generate_features_vector(dia,systo);
+
+%% Step 2 Define k + parameter setting
 k = 5;
 
+% parameters
+num_patients = numel(train_label);
 %% Step 3 & Step 4
 clc;
 test_d = [5.5 2.5]; % test data
