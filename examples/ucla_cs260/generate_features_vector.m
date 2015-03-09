8cca8aad5d408c0e566da571b9d4262d1eaded57
johnny <johnny5550822@gmail.com>
2015-03-09 15:51:07 -0700

    Add simple validation

---
+++ examples/ucla_cs260/generate_features_vector.m
@@ -0,0 +1,11 @@
+% Generate features vector from original data. There are several method of
+% generating features. 
+
+function fv = generate_features_vector(dia,systo)
+    %##########Method 1: Simple concatanation############
+    fv = [dia systo];
+
+
+
+
+end
