b31e017a9cc765a833160b5563c172f31a20885d
johnny5550822 <johnny5550822@gmail.com>
2015-03-10 11:58:28 -0700

    Add stack autoencoder codes

---
+++ examples/ucla_cs260/nn_main.m
@@ -0,0 +1,8 @@
+%% Created by King Chung Ho (Johnny Ho) in 03/09/15
+% For UCLA CS260 project (Winter 2015)
+% Objective: Use neural network(multi-layer) to build a model for prediction
+% Feature: 2 features (Diastolic and systolic) along different time
+% Number of training data: 39 patients, 20 with label 0, and 19 with label
+% 1
+
+
