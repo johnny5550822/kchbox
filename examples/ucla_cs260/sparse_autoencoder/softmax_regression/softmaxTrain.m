f3781b4e81a7c25269ebc6b84038dc235fe443c1
johnny5550822 <johnny5550822@gmail.com>
2015-03-11 10:37:49 -0700

    edit comment

--- examples/ucla_cs260/sparse_autoencoder/softmax_regression/softmaxTrain.m
+++ examples/ucla_cs260/sparse_autoencoder/softmax_regression/softmaxTrain.m
@@ -1,18 +1,6 @@
+% Train a softmax classifier
+
 function [softmaxModel] = softmaxTrain(inputSize, numClasses, lambda, inputData, labels, options)
-%softmaxTrain Train a softmax model with the given parameters on the given
-% data. Returns softmaxOptTheta, a vector containing the trained parameters
-% for the model.
-%
-% inputSize: the size of an input vector x^(i)
-% numClasses: the number of classes 
-% lambda: weight decay parameter
-% inputData: an N by M matrix containing the input data, such that
-%            inputData(:, c) is the cth input
-% labels: M by 1 matrix containing the class labels for the
-%            corresponding inputs. labels(c) is the class label for
-%            the cth input
-% options (optional): options
-%   options.maxIter: number of iterations to train for
 
 if ~exist('options', 'var')
     options = struct;
